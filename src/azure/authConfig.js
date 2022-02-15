2c1f6df287ae0b77703ecb07a494fc640c8a589f
dillonreid15 <dillonreid15@gmail.com>
2022-02-15 15:02:27 +0000

    Updated to use heroku url for redirect for hosting

--- src/azure/authConfig.js
+++ src/azure/authConfig.js
@@ -2,7 +2,7 @@ export const msalConfig = {
     auth: {
         clientId: "cf73ac9a-b612-43d8-9a4f-191991ae3dc9",
         authority: "https://login.microsoftonline.com/organizations/",
-        redirectUri: "http://localhost:3000/.auth/login/aad/callback",
+        redirectUri: "https://hons-peer-review-front-end.herokuapp.com/.auth/login/aad/callback",
     },
     cache: {
         cacheLocation: "sessionStorage",
